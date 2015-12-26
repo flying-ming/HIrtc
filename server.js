4eb4bc45ec39dbd1c3efba0f33d3211c1c3ed9a3
hufe <1150268339@qq.com>
2015-12-27 00:10:13 +0800

    12.27.0:09

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var SQL_TABLE = 'user_info';
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '1234',
+    password: 'hf1234',
     port: '3306'
 });
 
